9d96a0dc365e123cbf2124bdad649875e3fb3a55
Shubham-IX <shubhamh@incubxperts.com>
2022-03-14 12:04:26 +0530

    sonarcloud

---
+++ customer-location.ts
@@ -0,0 +1,73 @@
+import { Service } from 'typedi';
+import {
+    BaseResponse,
+    CustomerLocationOptions,
+    CustomerLocationResponse,
+    CustomerLocationCreateRequest,
+    HashMapResponse,
+    CustomerContactResponse,
+} from '../../core/entity';
+import { CustomerLocationServiceInterface } from '../../core/interfaces/services';
+
+@Service()
+export class CustomerLocation {
+    constructor(private readonly customerLocationService: CustomerLocationServiceInterface) {}
+
+    /**
+     * Returns collection of customers locations
+     * @param id for customer id
+     * @param customerLocationOptions for customer location query parameters
+     * @returns
+     */
+    async getByCustomerId(
+        id: string,
+        customerLocationOptions?: CustomerLocationOptions,
+    ): Promise<CustomerLocationResponse> {
+        return this.customerLocationService.getByCustomerId(id, customerLocationOptions);
+    }
+
+    /**
+     * Create or update customer location record.
+     * @param customerLocationCreateRequest for customer location details
+     * @returns
+     */
+    async post(customerLocationCreateRequest: CustomerLocationCreateRequest): Promise<CustomerLocationResponse> {
+        return this.customerLocationService.post(customerLocationCreateRequest);
+    }
+
+    /**
+     * Delete customer location
+     * @param id for customer location id
+     * @returns
+     */
+    async delete(id: string): Promise<BaseResponse> {
+        return this.customerLocationService.delete(id);
+    }
+
+    /**
+     * Returns collection of customers locations
+     * @param id for external id
+     * @returns
+     */
+    async getByExternalId(id: string): Promise<CustomerLocationResponse> {
+        return this.customerLocationService.getByExternalId(id);
+    }
+
+    /**
+     * Get a collection of contact for a Customer Location
+     * @param customerId for customer id
+     * @param customerLocationId for customer location id
+     * @returns
+     */
+     async getContactByCustomerId(customerId: string, customerLocationId: string): Promise<CustomerContactResponse> {
+        return this.customerLocationService.getContactByCustomerId(customerId, customerLocationId);
+    }
+
+    /**
+     * Returns a dictionary for mapping 'Status' keys to labels on Customer Location objects.
+     * @returns
+     */
+    async getMetaStatus(): Promise<HashMapResponse> {
+        return this.customerLocationService.getMetaStatus();
+    }
+}
